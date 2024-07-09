cada17a652979b48cd6f2694bd61320d8f023597
CanvasL <746591811@qq.com>
2024-07-09 12:34:25 +0800

    feat: move device mapping logic to factory

--- src/Product.sol
+++ src/Product.sol
@@ -10,7 +10,7 @@ contract Product is Initializable, ERC721Upgradeable {
 
     address public ADMINISTRATOR;
     string public BASE_TOKEN_URI;
-    uint256 private _tokenIdCount = 1;
+    uint256 private _tokenIdCount = 0;
     mapping(address => uint256) internal _tokenIdByDevice;
     mapping(uint256 => address) internal _deviceByTokenId;
 
@@ -39,36 +39,10 @@ contract Product is Initializable, ERC721Upgradeable {
         _;
     }
 
-    function registerDevices(
-        address[] memory devices
-    ) public onlyAdministrator {
-        for (uint256 i = 0; i < devices.length; ++i) {
-            _tokenIdByDevice[devices[i]] = _tokenIdCount;
-            _deviceByTokenId[_tokenIdCount++] = devices[i];
-        }
-    }
-
-    function mint(address to, address device) public onlyAdministrator {
-        uint256 tokenId = _tokenIdByDevice[device];
-        if (tokenId == 0) {
-            revert DeviceNotRegistered();
-        }
+    function mint(address to) public onlyAdministrator returns (uint256) {
+        uint256 tokenId = _tokenIdCount++;
         _safeMint(to, tokenId);
-    }
-
-    function mint(address to, uint256 tokenId) public onlyAdministrator {
-        if (_deviceByTokenId[tokenId] == address(0)) {
-            revert DeviceNotRegistered();
-        }
-        _safeMint(to, tokenId);
-    }
-
-    function getTokenIdByDevice(address device) public view returns (uint256) {
-        return _tokenIdByDevice[device];
-    }
-
-    function getDeviceByTokenId(uint256 tokenId) public view returns (address) {
-        return _deviceByTokenId[tokenId];
+        return tokenId;
     }
 
     function _baseURI() internal view override returns (string memory) {
