1bc37b5f54a1532b71812b2916f0cb35479dd282
gaowhen <gaowhen.com@gmail.com>
2024-08-16 10:06:58 +0800

    remove env

--- explorer-ui/src/env.js
+++ explorer-ui/src/env.js
@@ -17,7 +17,6 @@ export const env = createEnv({
    */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
-    NEXT_PUBLIC_ENV: z.string(),
     NEXT_PUBLIC_GRAPHQL_URI: z.string(),
   },
 
@@ -28,7 +27,6 @@ export const env = createEnv({
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
-    NEXT_PUBLIC_ENV: process.env.NEXT_PUBLIC_ENV,
     NEXT_PUBLIC_GRAPHQL_URI: process.env.NEXT_PUBLIC_GRAPHQL_URI,
   },
   /**
